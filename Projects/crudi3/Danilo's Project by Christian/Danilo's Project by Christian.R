c5f73790126791caf6daa93143840e99c822e8c0
crudi3 <rudi.christian3397gmail.com>
2023-06-06 03:40:57 +0200

    More Datawrangling and for-loop to count the occurances of Artists
    Problem to be Solved: Strings with () cause problems with the str_count() and regex() function --> replace?

--- Projects/crudi3/Danilo's Project by Christian/Danilo's Project by Christian.R
+++ Projects/crudi3/Danilo's Project by Christian/Danilo's Project by Christian.R
@@ -1,5 +1,5 @@
 ## Initial Solution for Danilo's project
-
+library(tidyverse)
 # Load Dataset
 library(readxl)
 PG_BD_INVITACIONES_SM05_20_2021 <- read_excel("Projects/danilopenagos/PG_BD_INVITACIONES_SM05-20-2021.xlsx")
@@ -19,15 +19,13 @@ names(df)
 
 # Create a subset dataset using the variables: "Nombre / Galeria / Espacio", "Artistas", 
 data <- subset(df, select = c("Nombre / Galeria / Espacio", "Artistas"))
-typeof(data$Artistas)
 
+# splitting the strings of Artistas and saving as new variable
+data1<-data |> separate_rows(Artistas, sep = ", ")
+data1
 
-#spliting the strings of Artistas and saving as new variable
+# removing all the cases with NA
+artgal<-data1[complete.cases(data1), ]
+artgal
 
-data$SingleArtist <- strsplit(data$Artistas, split = ", ")
-
-unlist(data$SingleArtist)
-
-#done so far: merging dataset, creating a subset df, splitting the artists
-
-# to do: create a new df with each artist appearing only once --> New Artist Variable
+# for-schleife erstellen, die zählt wie oft ein Künstler in einer einzigartigen Gallerie vertreten war
