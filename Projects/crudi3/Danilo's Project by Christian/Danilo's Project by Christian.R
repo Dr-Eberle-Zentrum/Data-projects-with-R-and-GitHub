da29a126bb8d15145961c600a7222b01b4a4ad15
crudi3 <rudi.christian3397gmail.com>
2023-06-07 02:02:59 +0200

    Some alterations

--- Projects/crudi3/Danilo's Project by Christian/Danilo's Project by Christian.R
+++ Projects/crudi3/Danilo's Project by Christian/Danilo's Project by Christian.R
@@ -3,7 +3,6 @@ library(tidyverse)
 # Load Dataset
 library(readxl)
 PG_BD_INVITACIONES_SM05_20_2021 <- read_excel("Projects/danilopenagos/PG_BD_INVITACIONES_SM05-20-2021.xlsx")
-View(PG_BD_INVITACIONES_SM05_20_2021)
 
 path <- "Projects/danilopenagos/PG_BD_INVITACIONES_SM05-20-2021.xlsx"
 
@@ -29,3 +28,27 @@ artgal<-data1[complete.cases(data1), ]
 artgal
 
 # for-schleife erstellen, die zählt wie oft ein Künstler in einer einzigartigen Gallerie vertreten war
+length(artgal$Artistas)
+artgal$AnzahlAusstellungen <- 0
+x=0
+for (i in artgal$Artistas) {
+  x=x+1
+  artgal$AnzahlAusstellungen[x]<- sum(str_count(artgal$Artistas, pattern=i))
+  
+}
+# Problem: for schleife bricht frühzeitig ab bei dem Wert "Confabularios ( : ..." --> Vermutlich wegen der Klammer, ohne diese funktiuniert der Befehl
+sum(str_count(artgal$Artistas, pattern="Confabularios"))
+
+# to do: push die Project Lösung in den Ordner von Danilo aber mein Branch benutzen
+#      - pull request erstellen (in Github)
+                
+
+# grouping befehl group_by()
+
+#data.frame(
+#A = 1:4,
+#B = c(1,1,2,2)
+#) |> 
+#  group_by(B) |>
+#  # count()
+#  summarize( anzahl = n() )
