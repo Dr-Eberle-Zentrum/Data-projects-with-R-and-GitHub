8ef151cae74647ace4bc31809846d6586545ff0a
NoelStudentAcc <noel.wytopil@student.uni-tuebingen.de>
2023-12-04 19:50:21 +0100

    a bit new progress

--- Projects/YunjiKang/NoelStudentAcc_Rskript for Tests.R
+++ Projects/YunjiKang/NoelStudentAcc_Rskript for Tests.R
@@ -25,6 +25,39 @@ ggplot() +
 
 ### Task 2 ---------------------------------------------------------------------
 
+crime_task2 <- crimedata2 |> 
+  select(c("geo", "TIME_PERIOD", "OBS_VALUE")) |> 
+  mutate(Type = "Crime")
+
+income_task2 <- incomedata |> 
+  select(c("geo", "TIME_PERIOD", "OBS_VALUE")) |> 
+  mutate(Type = "Income")
+
+poverty_task2 <- povertydata |> 
+  select(c("geo", "TIME_PERIOD", "OBS_VALUE")) |> 
+  mutate(Type = "Poverty")
+
+datatask2 <- merge(crime_task2, income_task2, by = "Type")
+
+ggplot(aes(x=gdpPercap, y=lifeExp, size=pop, color=continent)) +
+  geom_point(alpha=0.5) +
+  scale_size(range = c(.1, 24), name="Population (M)")
+
+
+
+# The dataset is provided in the gapminder library
+library(gapminder)
+data <- gapminder %>% filter(year=="2007") %>% dplyr::select(-year)
+
+# Most basic bubble plot
+data %>%
+  arrange(desc(pop)) %>%
+  mutate(country = factor(country, country)) %>%
+  ggplot(aes(x=gdpPercap, y=lifeExp, size=pop, color=continent)) +
+  geom_point(alpha=0.5) +
+  scale_size(range = c(.1, 24), name="Population (M)")
+
+
 
 ## Tests -----------------------------------------------------------------------
 
