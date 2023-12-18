be98c35e0b8e3ee1019554469ee0d6dbfd1409f6
blubbi90 <nogli90@gmx.de>
2023-12-18 23:27:53 +0100

    simple Introduction Part I (without shiny content)

---
+++ Projects/JanaKimmich/Nicos_solution/install_packages_simple.R
@@ -0,0 +1,10 @@
+#install_packages
+
+required_packages <- c("tidyverse", "plotly", "usethis", "emo")
+
+new_packages <- required_packages[!(required_packages %in% installed.packages()[,"Package"])]
+
+if(length(new_packages) > 0) {
+  install.packages(new_packages, dependencies = FALSE, repos = NULL) # <- repos = Null for shiy document, loding repo direkt from cran
+}
+
