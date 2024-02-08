27bace9b1ef9902f37615dd1d9c5b7add2e93bd6
timow98 <timowalch@web.de>
2024-02-08 11:47:41 +0100

    R Script with setup steps

---
+++ timow98/Setup.R
@@ -0,0 +1,15 @@
+#* Configure R Studio. Install needed package
+#* install.packages("usethis")
+#* library(usethis)
+
+#* Log in
+#* use_git_config(user.name = "timow98", user.email = "timowalch@web.de")
+
+#* use "main" instead of "master"
+#* usethis::git_default_branch_configure()
+
+#* Create personal token
+#* create_github_token(description="R2 course")
+
+#* Give token to R
+#* credentials::set_github_pat(force_new=TRUE)
