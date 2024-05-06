a67ee9fb975e6809a8fc8a43f2ceb46d6aef71e7
martin-raden <mmann@informatik.uni-freiburg.de>
2024-05-06 15:29:41 +0200

    drop obsolete stuff

--- Projects/martin-raden/martin.R
+++ Projects/martin-raden/martin.R
@@ -5,17 +5,18 @@ library(tidyverse)
 data <-
   read_csv("https://ourworldindata.org/uploads/2022/09/Mammal-comeback-in-Europe-data-OWID-download.csv") |>
   # reduce column names to first single word
+  # (!) implicit function definition
   rename_with(~str_extract(., "^\\w+")) |>
   # clean up
   mutate(
+    # (!) cut of last character for integer conversion
     Average = str_sub(Average, 1,-2) %>% as.integer(),
-    Min = 1,
-    yIdx = rank(Average),
-    # set order of names based on Average values for later plotting
+    # (!) set order of names based on Average values for later plotting
     Common = factor(Common) %>% fct_reorder(Average),
     Range = str_c(Start," to ", End)
     ) %>%
-  arrange(yIdx)
+  # drop obsolete columns
+  select(-Start,-End)
 
 # load bear image from local file
 # library(png)
@@ -28,31 +29,35 @@ library(magick)
 library(patchwork)
 
 data %>%
-  ggplot(aes(x=Min, y= Common, yend= Common)) +
+  ggplot(aes(y= Common, yend= Common)) +
   theme_minimal()+
   # disable horizontal grid lines
   theme(panel.grid.major.y = element_blank(),
         panel.grid.minor.y = element_blank()) +
-  geom_segment(aes(xend = 0.93*Average),
+  # (!) using constant 1 as start x coordinate
+  geom_segment(aes(x=1, xend = 0.93*Average),
                col="gray",
                arrow = arrow(ends = "last", length = unit(5,"pt"), type = "closed"),
                arrow.fill = "gray") +
-  geom_point(col="ivory4") +
+  geom_point(aes(x=1), col="ivory4") +
   geom_point(aes(x=Average), col="lightskyblue4") +
   scale_x_log10(breaks = 10^(0:4),
-                # use a scales-function to generate a function for label generation
-                labels = scales::percent_format(scale = 1, big.mark=",")(10^(0:4)),
+                # (!) use a scales-function to generate a function for label generation
+                labels = scales::percent_format(scale = 1, big.mark=","),
                 limits=c(1,10^5)) +
   labs(
     x = "Average relative change in size of population (logarithmic scale)",
-    y = NULL
+    y = NULL # (!) drop y axis label
   ) +
   # disable clipping to allow for annotations outside of plot area
   coord_cartesian(clip = "off")+
   # year range annotations
-  geom_label(aes(label = Range, x=Min+0.1), hjust=0, size=3, fill="white", label.size=0, col="darkgray" ) +
+  geom_label(aes(label = Range, x=1.1),
+             hjust=0, size=3, fill="white", label.size=0, col="darkgray" ) +
   # average change annotations after end points
-  geom_text(aes(label= scales::percent_format(scale = 1, big.mark=",")(Average), x=Average*1.1), hjust=0, size=3)+
+  geom_text(aes(label= scales::percent_format(scale = 1, big.mark=",")(Average),
+                x=Average*1.1),
+            hjust=0, size=3)+
   # curved annotation line for bear population
   annotate(
     geom = "curve", x = 150, xend=85, y = 4, yend = 4, col = "darkgray",
