95dd2cc863263bfb87220449023d4ab3e9916943
martin-raden <mmann@informatik.uni-freiburg.de>
2024-12-04 11:23:06 +0100

    first solution

--- Projects/GulmiraKairr/martin.R
+++ Projects/GulmiraKairr/martin.R
@@ -8,4 +8,22 @@ rstudioapi::getSourceEditorContext()$path |>
   setwd()
 
 "adult-depression.csv" |>
-  read_csv()
+  read_csv() ->
+  data
+
+data |>
+  group_by(Year, Strata) |>
+  mutate( AnnualFreq = Frequency / sum(Frequency)) |>
+  select(Year, Strata, `Strata Name`, Frequency, AnnualFreq) |>
+  # filter( Strata %in% c("Education")) |>
+  filter( Strata %in% c("Education","Race-Ethnicity","Age")) |>
+  ggplot(aes(x=Year,y=AnnualFreq, colour=`Strata Name`, shape=Strata)) +
+  # labs( colour="C", shape="C") + # not working since different labels
+  guides(shape=F)+
+  geom_line()+
+  geom_point() +
+  facet_wrap(vars(Strata)
+             , nrow=1
+             , scales = "free"
+             )
+# so far no straight idea how to split/join the legend information per facet
