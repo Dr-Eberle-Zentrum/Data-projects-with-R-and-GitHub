6743c68ebca6296463f16a9e2b22457490a7c17e
NicolasDelgadoL <delgado.lozano.nico@gmail.com>
2024-12-05 17:27:35 +0100

    first two plots

--- Projects/NicolasDelgadoL/Sun's data project by Nicolás.R
+++ Projects/NicolasDelgadoL/Sun's data project by Nicolás.R
@@ -3,18 +3,35 @@
 
 install.packages("here")
 library(here)
+library(dplyr)
+library(ggplot2)
 setwd(here())
 getwd()
-data <- read.csv("../SunKyoung Moon/shopping_trends_updated.csv")
-data <- read.csv(here("R & Github", "Projects", "SunKyoung Moon", "student_lifestyle_dataset.csv"))
+data <- read.csv("../R & Github/Projects/SunKyoung Moon/shopping_trends_updated.csv")
 
 # Data Exploration
 View(data)
 hist(data$Age)
 
 # Graph 1: Female consumers’ purchase amount by age group
-data <- data$age_by_group 
-data$age_by_group <- c( )
+processed_data <- data %>% 
+  mutate(age_group = cut(Age, seq(18, 70, by = 5), right = FALSE)#,
+         #labels = c("18-22", "23-27", "28-32", "33-37", "38-42", "43-47", 
+         #           "48-52", "53-57", "58-62", "63-67", "68-70")
+        ) %>%
+  group_by(Gender,age_group) %>%
+  summarize(TotalPurchase = sum(Purchase.Amount..USD.))
+
+ggplot(filter(processed_data, Gender == "Female"), aes(x = age_group, y = TotalPurchase)) +
+  geom_bar(stat = "identity", position = "dodge", fill = "blue")
+  
+
+ggplot(filter(processed_data, Gender == "Male"), aes(x = age_group, y = TotalPurchase)) +
+  geom_bar(stat = "identity", position = "dodge", fill = "blue")
+
+View(processed_data)
+  
+
 # Graph 2: Male consumers’ purchase amount by age group
 
 # Graph 3: Female consumers’ frequency of purchases by age group
--- Projects/SunKyoung Moon/NicolasDelgadoL.md
+++ Projects/SunKyoung Moon/NicolasDelgadoL.md
@@ -15,8 +15,26 @@
 
 # Graph 1: Female consumers’ purchase amount by age group
 
-    data <- data$age_by_group 
-    data$age_by_group <- c()
+    processed_data <- data %>% 
+      mutate(age_group = cut(Age, seq(18, 70, by = 5), right = FALSE)#,
+             #labels = c("18-22", "23-27", "28-32", "33-37", "38-42", "43-47", 
+             #           "48-52", "53-57", "58-62", "63-67", "68-70")
+            ) %>%
+      group_by(Gender,age_group) %>%
+      summarize(TotalPurchase = sum(Purchase.Amount..USD.))
+
+    ## `summarise()` has grouped output by 'Gender'. You can override using the
+    ## `.groups` argument.
+
+    ggplot(filter(processed_data, Gender == "Female"), aes(x = age_group, y = TotalPurchase)) +
+      geom_bar(stat = "identity", position = "dodge", fill = "blue")
+
+![](NicolasDelgadoL_files/figure-markdown_strict/graphs-1.png)
+
+    ggplot(filter(processed_data, Gender == "Male"), aes(x = age_group, y = TotalPurchase)) +
+      geom_bar(stat = "identity", position = "dodge", fill = "blue")
+
+![](NicolasDelgadoL_files/figure-markdown_strict/graphs-2.png)
 
 ### Graph 2: Male consumers’ purchase amount by age group
 
